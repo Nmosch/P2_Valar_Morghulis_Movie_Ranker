c3c0fc0b54e1d1c500ef02bd56f36f7d1fec9b39
Nathan Darnell <darnell.nathaniel95@gmail.com>
2020-06-08 17:30:29 -0700

    changed front page from 5 inputs to 1, added a background image and changed some styling

--- public/js/script.js
+++ public/js/script.js
@@ -7,38 +7,6 @@ $( document ).ready(function() {
         $("#selectedRatingOne").text($(this).text())
     })
 
-    $("#secondMovieGenre a").click(function(){
-        $("#selectedGenreTwo").text($(this).text())
-    })
-
-    $("#secondMovieRating a").click(function(){
-        $("#selectedRatingTwo").text($(this).text())
-    })
-
-    $("#thirdMovieGenre a").click(function(){
-        $("#selectedGenreThree").text($(this).text())
-    })
-
-    $("#thirdMovieRating a").click(function(){
-        $("#selectedRatingThree").text($(this).text())
-    })
-
-    $("#fourthMovieGenre a").click(function(){
-        $("#selectedGenreFour").text($(this).text())
-    })
-
-    $("#fourthMovieRating a").click(function(){
-        $("#selectedRatingFour").text($(this).text())
-    })
-
-    $("#fifthMovieGenre a").click(function(){
-        $("#selectedGenreFive").text($(this).text())
-    })
-
-    $("#fifthMovieRating a").click(function(){
-        $("#selectedRatingFive").text($(this).text())
-    })
-
     $("#saveOptions").click(function(){
         console.log("Working")
         
